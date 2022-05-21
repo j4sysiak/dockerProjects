21f6296284443b41db7b4918a38ed5e031ef4d10
Jacek Sysiak <j4sysiak@gmail.com>
2022-05-21 11:30:19 +0200

    21-05-2021 11:30

---
+++ docker-crash-course/src/main/java/com/example/dockercrashcourse/HelloWorldBean.java
@@ -0,0 +1,15 @@
+package com.example.dockercrashcourse;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class HelloWorldBean {
+
+    private String message;
+
+}
