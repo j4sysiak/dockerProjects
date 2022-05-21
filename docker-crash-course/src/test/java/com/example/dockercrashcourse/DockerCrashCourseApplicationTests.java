8bf6f127ba3c6e81f47570b56f485f8518a759a7
Jacek Sysiak <j4sysiak@gmail.com>
2022-05-21 11:17:47 +0200

    21-05-2022 11:17

---
+++ docker-crash-course/src/test/java/com/example/dockercrashcourse/DockerCrashCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockercrashcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCrashCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
