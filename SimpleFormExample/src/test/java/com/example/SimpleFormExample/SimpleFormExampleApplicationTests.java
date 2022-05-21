50c2e6b19a7fe2bdfe2acf00de1925f6c6d90d65
Jacek Sysiak <j4sysiak@gmail.com>
2022-05-21 11:21:38 +0200

    21-05-2021 11:21

---
+++ SimpleFormExample/src/test/java/com/example/SimpleFormExample/SimpleFormExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleFormExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFormExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
