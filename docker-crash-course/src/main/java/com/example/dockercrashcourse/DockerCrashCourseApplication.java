bc07dfdc8da6588ac24d026aa65d4502a2166358
Jacek Sysiak <j4sysiak@gmail.com>
2022-05-22 03:59:50 +0000

    qq22222222

--- docker-crash-course/src/main/java/com/example/dockercrashcourse/DockerCrashCourseApplication.java
+++ docker-crash-course/src/main/java/com/example/dockercrashcourse/DockerCrashCourseApplication.java
@@ -3,10 +3,14 @@ package com.example.dockercrashcourse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DockerCrashCourseApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DockerCrashCourseApplication.class, args);
+
 	}
 }
