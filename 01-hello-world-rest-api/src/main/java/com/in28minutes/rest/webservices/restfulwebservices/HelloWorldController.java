a54faf077fa1d951e7a9e55b7ca190ff32742582
Jacek Sysiak <j4sysiak@gmail.com>
2022-08-28 16:40:47 +0200

    27-08-2022 16:38

--- 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	//    https://localhost/hello-world
+	//    http://localhost:8080/hello-world
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World v10";
 	}
 
-	//    https://localhost/hello-world-bean
+	//    http://localhost:8080/hello-world-bean
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
@@ -21,7 +21,7 @@ public class HelloWorldController {
 		return new HelloWorldBean("Hello World v10");
 	}
 
-	//   https://hello-world/path-variable/in28minutes
+	//   http://localhost:8080/hello-world/path-variable/in28minutes
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World v10, %s", name));
