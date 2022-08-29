36f4a87e5d0a7adbfe9c20c21d2d3cedbba74d50
Jacek Sysiak <j4sysiak@gmail.com>
2022-08-29 16:11:41 +0200

    29-08-2022 16:11

---
+++ springboot-client-docker/src/test/java/com/example/springbootclientdocker/SpringbootClientDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootclientdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootClientDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
