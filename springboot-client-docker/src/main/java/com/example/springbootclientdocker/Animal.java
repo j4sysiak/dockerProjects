ce266fc4262a5426dc1c8ab100436073c8a2159d
Jacek Sysiak <j4sysiak@gmail.com>
2022-08-30 07:14:49 +0200

    30-08-2022 07:04

--- springboot-client-docker/src/main/java/com/example/springbootclientdocker/Animal.java
+++ springboot-client-docker/src/main/java/com/example/springbootclientdocker/Animal.java
@@ -1,5 +1,12 @@
 package com.example.springbootclientdocker;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 @Setter
 @Getter
--- springboot-client-docker/src/main/java/com/example/springbootclientdocker/AnimalClient.java
+++ springboot-client-docker/src/main/java/com/example/springbootclientdocker/AnimalClient.java
@@ -5,13 +5,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+//@Controller
+@RestController
 public class AnimalClient {
 
     //end point
     @GetMapping("/showForGui")
+    @ResponseBody
     public Animal[] get() {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<Animal[]> exchange = restTemplate.exchange("http://localhost:9090/animals",
