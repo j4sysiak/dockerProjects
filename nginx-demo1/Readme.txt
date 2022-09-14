bc2682a213ea84307837ca27dbdfc60cb4618856
Jacek Sysiak <j4sysiak@gmail.com>
2022-09-14 21:45:14 +0200

    14-09-2022 21:44

---
+++ nginx-demo1/Readme.txt
@@ -0,0 +1,4 @@
+Spring Boot Nginx Reverse Proxy on Localhost Development
+https://www.youtube.com/watch?v=O7nfg4URAyo
+
+https://github.com/ekim197711/springboot-nginx-localhost-development/tree/master/script
--- nginx-demo1/src/main/java/com/example/nginxdemo1/MyTrainRestController.java
+++ nginx-demo1/src/main/java/com/example/nginxdemo1/MyTrainRestController.java
@@ -1,20 +1,20 @@
 package com.example.nginxdemo1;
 
 
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@Slf4j
+//@Slf4j
 public class MyTrainRestController {
 
     // end point: http://localhost:8080/api/timetable
     @GetMapping("/timetable")
     public String timetable(){
-        log.info("Serve timetable...");
+//        log.info("Serve timetable...");
         return "Train comes soon!";
     }
 }
