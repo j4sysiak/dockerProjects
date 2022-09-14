8f70e194991c5b2b4ca0dcaebdbc6c9e8b22357b
Jacek Sysiak <j4sysiak@gmail.com>
2022-09-14 19:47:46 +0200

    14-09-2022 19:47

---
+++ nginx-demo1/src/main/java/com/example/nginxdemo1/NginxDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.nginxdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NginxDemo1Application.class, args);
+	}
+
+}
