434ddf9960479b0ca30076141ffa00a9d24c83f7
Jacek Sysiak <j4sysiak@gmail.com>
2022-09-18 12:10:54 +0200

    18-09-2022 12:09

---
+++ springboot-docker-sdk-install-on-ubuntu/src/main/java/com/example/springbootdockersdkinstallonubuntu/SpringbootDockerSdkInstallOnUbuntuApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockersdkinstallonubuntu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerSdkInstallOnUbuntuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerSdkInstallOnUbuntuApplication.class, args);
+	}
+
+}
