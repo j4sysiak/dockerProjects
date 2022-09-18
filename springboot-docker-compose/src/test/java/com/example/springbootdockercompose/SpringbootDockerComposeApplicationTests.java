a841ea2923bd83967cf4a3de9a1a800f64ef30f2
Jacek Sysiak <j4sysiak@gmail.com>
2022-09-18 08:46:11 +0200

    18-09-2022 08:45

---
+++ springboot-docker-compose/src/test/java/com/example/springbootdockercompose/SpringbootDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
