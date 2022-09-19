c364ac873c17210ad607640e0fb8023dca78a93f
Jacek Sysiak <j4sysiak@gmail.com>
2022-09-19 06:52:19 +0200

    19-09-2022 06:48

---
+++ demo-docker-multistage-build/src/main/java/com/example/demodockermultistagebuild/DemoDockerMultistageBuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodockermultistagebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerMultistageBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerMultistageBuildApplication.class, args);
+	}
+
+}
