4cb5102766406da4022d42906a19911ea0a1da21
Jacek Sysiak <j4sysiak@gmail.com>
2022-09-24 07:58:51 +0200

    24-09-2022 07:58

---
+++ rabbitmq-high-availability-with-nginx/src/main/java/com/example/rabbitmqhighavailabilitywithnginx/RabbitmqHighAvailabilityWithNginxApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqhighavailabilitywithnginx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqHighAvailabilityWithNginxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqHighAvailabilityWithNginxApplication.class, args);
+	}
+
+}
