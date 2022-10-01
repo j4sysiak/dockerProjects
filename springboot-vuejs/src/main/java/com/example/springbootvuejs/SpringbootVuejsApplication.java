5ff785d90fc6110583ebe40cd125f9bf18bd54b0
Jacek Sysiak <j4sysiak@gmail.com>
2022-10-01 19:38:53 +0200

    01-10-2022 19:38

---
+++ springboot-vuejs/src/main/java/com/example/springbootvuejs/SpringbootVuejsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVuejsApplication.class, args);
+	}
+
+}
