73a90a186fd96b8e7cf02a99fcf7c1f0738790a5
Jacek Sysiak <j4sysiak@gmail.com>
2022-10-02 08:15:24 +0200

    02-10-2022 08:14

---
+++ springboot-vuejs/src/main/java/com/example/springbootvuejs/TestController.java
@@ -0,0 +1,16 @@
+package com.example.springbootvuejs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    //   http://localhost:8080/api/test
+    //  docker:   http://localhost:9000/api/test
+    @GetMapping(path="/api/test")
+    public String destination(){
+        return "TEST";
+    }
+}
