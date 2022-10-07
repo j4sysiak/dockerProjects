f57bee7f594826e22abfd6e2ba7afb2dd68dd857
Jacek Sysiak <j4sysiak@gmail.com>
2022-10-07 06:38:35 +0200

    07-10-2022 06:35

--- springboot-artemis/src/main/java/com/example/springbootartemis/GreetingController.java
+++ springboot-artemis/src/main/java/com/example/springbootartemis/GreetingController.java
@@ -12,16 +12,18 @@ import org.springframework.web.bind.annotation.*;
 public class GreetingController {
     private final JmsTemplate jmsTemplate;
 
-    // http://localhost:8080/greeting/test
+    // http://localhost:8080/greeting/getting
     @GetMapping("/getting")
     public String test() {
-//        log.info("Send this message to jms queue");
+        log.info("Send this message to jms queue");
         return "test: GreetingController from GetMapping";
     }
 
-    @PostMapping("/posting")
+
+    //   http://localhost:8080/greeting/
+    @PostMapping("/")
     public String sendMessage(@RequestBody String message) {
-//        log.info("Send this message to jms queue: " + message);
+        log.info("Send this message to jms queue: " + message);
 //        jmsTemplate.convertAndSend("myqueue2", message);
         return "Message sent " + message;
     }
