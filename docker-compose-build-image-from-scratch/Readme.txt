03c9517e4b852817abe0f49caf223912c221776d
Jacek Sysiak <j4sysiak@gmail.com>
2022-10-10 07:11:26 +0200

    10-10-2022 07:10

---
+++ docker-compose-build-image-from-scratch/Readme.txt
@@ -0,0 +1,9 @@
+Docker Compose Build Image From Scratch From Alpine
+
+https://www.youtube.com/watch?v=elzo_ub1CG0&list=PLF5X0J2bZ_k44MMIJvncXgJkzy0KxBGMB&index=24
+
+
+$ docker-compose build
+$ docker-compose up
+$ docker-compose run mikesapp sh
+/app # bash
---
+++ docker-compose-build-image-from-scratch/src/main/java/com/example/dockercomposebuildimagefromscratch/DockerComposeBuildImageFromScratchApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockercomposebuildimagefromscratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposeBuildImageFromScratchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerComposeBuildImageFromScratchApplication.class, args);
+	}
+
+}
